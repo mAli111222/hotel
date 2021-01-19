18b0b80634e2f4c7135695a9e4da35fcdde70fe4
twoody <tanner.l.woody@gmail.com>
2021-01-19 08:39:41 -0800

    setting up AppSection vuex state MGMT

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,44 @@ import Vuex from "vuex"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	actions: {},
-	modules: {},
-	mutations: {},
-	state: {},
+	actions:
+	{},
+	getters:
+	{},
+	modules:
+	{},
+	mutations:
+	{
+		hideBottomBanner (state)
+		{
+			state.isShowingBottomBanner = false
+		},
+		hideNav (state)
+		{
+			state.isShowingNav = false
+		},
+		hideTopBanner (state)
+		{
+			state.isShowingTopBanner = false
+		},
+
+		showBottomBanner (state)
+		{
+			state.isShowingBottomBanner = true
+		},
+		showNav (state)
+		{
+			state.isShowingNav = true
+		},
+		showTopBanner (state)
+		{
+			state.isShowingTopBanner = true
+		},
+
+	},
+	state: {
+		isShowingBottomBanner: false,
+		isShowingNav: true,
+		isShowingTopBanner: false,
+	},
 })
