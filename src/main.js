22f6c6470073e02e5af2345725bc9d32bc9039c1
Tanner Woody <tanner.l.woody@gmail.com>
2021-01-21 09:26:43 -0800

    firestorePlugin via VueFire

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
+import { firestorePlugin } from "vuefire"
 
 Vue.config.productionTip = false
 
 new Vue({
+	firestorePlugin, 
 	router,
 	render: (h) => h(App),
 }).$mount("#app")
