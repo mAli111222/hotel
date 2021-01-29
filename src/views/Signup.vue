87ef182b8bd1daa59ba928b5b866571b0311586c
Tanner Woody <tanner.l.woody@gmail.com>
2021-01-28 19:13:53 -0800

    sign up page

---
+++ src/views/Signup.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="page-wrapper">
+	</div>
+</template>
+
+<script>
+export default {
+	name: "",
+
+	props: {},
+	computed:
+	{
+
+	},
+	methods: 
+	{
+
+	},
+}
+</script>
+
+<style scoped lang="less">
+foo-wrapper {
+
+}
+</style>
+
