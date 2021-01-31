26862395f3f1fd036cf4dd82089454bdcee5a97d
twoody <tanner.l.woody@gmail.com>
2021-01-30 23:03:48 -0800

    start to setup homepage with search form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<AvailabilitySearch />
 	</div>
 </template>
 
 <script>
 
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
+import AvailabilitySearch from "@/components/AvailabilitySearch.vue"
 
 export default {
 	name: "Home",
 	components: {
-		HelloWorld,
+		AvailabilitySearch,
 	},
 	created: function()
 	{
