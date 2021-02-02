1e10f664550c3175f3584fc3298414dce2b5b6ec
twoody <tanner.l.woody@gmail.com>
2021-02-02 00:03:24 -0800

    setting up firebase config
    trying: https://blog.logrocket.com/vue-firebase-authentication/

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store/store.js"
+import * as firebaes from "firebase"
+
+const firebaseConfig = {
+
+}
 
 Vue.config.productionTip = false
 
